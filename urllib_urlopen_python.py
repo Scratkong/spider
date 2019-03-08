0c8c77a6280ae8268d3b258caa5983b3b2f3c3d5
scratkong <scratkong@163.com>
2019-03-08 23:10:16 +0800

    add time and data prarameter

--- urllib_urlopen_python.py
+++ urllib_urlopen_python.py
@@ -1,13 +1,26 @@
 import urllib.request
+import urllib.parse
 
-response = urllib.request.urlopen('https://www.python.org')
+# response = urllib.request.urlopen('https://www.python.org')
 
-print(type(response))
+# print(type(response))
+#
+# print(response.read().decode('utf-8'))
+#
+# print(response.status)
+#
+# print(response.getheaders())
+#
+# print(response.getheader('Server'))
 
-print(response.read().decode('utf-8'))
 
-print(response.status)
+# data 参数
+data = bytes(urllib.parse.urlencode({'word':'hello'}), encoding='utf8')
+response = urllib.request.urlopen('http://httpbin.org/post', data = data)
+print(response.read())
 
-print(response.getheaders())
 
-print(response.getheader('Server'))
+# timeout 参数
+response = urllib.request.urlopen('http://httpbin.org/get', timeout=1)
+print(response.read())
+
