11fb56e7b1707dde62f807091442d25c4bb01cf9
scratkong <scratkong@163.com>
2019-03-08 22:55:03 +0800

    urllib demo

---
+++ urllib_urlopen_python.py
@@ -0,0 +1,13 @@
+import urllib.request
+
+response = urllib.request.urlopen('https://www.python.org')
+
+print(type(response))
+
+print(response.read().decode('utf-8'))
+
+print(response.status)
+
+print(response.getheaders())
+
+print(response.getheader('Server'))
